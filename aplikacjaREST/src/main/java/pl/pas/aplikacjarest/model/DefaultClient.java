8a4a65f657f9a8155973c02ad85ac90b31c39b5e
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-11-10 15:03:33 +0100

    Added registerClient method and Client DTOs.

--- aplikacjaREST/src/main/java/pl/pas/aplikacjarest/model/DefaultClient.java
+++
@@ -1,7 +0,0 @@
-package pl.pas.aplikacjarest.model;
-
-public class DefaultClient extends Client {
-    public DefaultClient(String firstName, String lastName, String username, String email, String password) {
-        super(firstName, lastName, username, email, password);
-    }
-}
