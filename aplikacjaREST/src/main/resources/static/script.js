a89de3a5013dadda0ab15221587807c3252e0fef
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-11-11 17:54:57 +0100

    Added basic start form.

---
+++ aplikacjaREST/src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    window.location.href = '/login.html';
+});
+
+document.getElementById('registerBtn').addEventListener('click', function() {
+    window.location.href = '/register.html';
+});
