06a409466fc2cdb736a8c3ed70310df3634655bb
Wojtyla23 <247734@edu.p.lodz.pl>
2024-12-03 21:41:47 +0100

    Javascript validation and confirm button

--- aplikacjaMVC/src/main/resources/static/script.js
+++ aplikacjaMVC/src/main/resources/static/script.js
@@ -1,1 +1,30 @@
-document.getElementById("current-page").textContent = `Title: ${document.title}`;
+document.getElementById("current-page").textContent = `${document.title}`;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#rentForm1");
+    if (!form) {
+        return;
+    }
+
+    form.addEventListener("submit", function (event) {
+        const userConfirmed = confirm("OK?");
+        if (!userConfirmed) {
+            event.preventDefault();
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#rentForm2");
+    if (!form) {
+        return;
+    }
+
+    form.addEventListener("submit", function (event) {
+        const userConfirmed = confirm("OK?");
+        if (!userConfirmed) {
+            event.preventDefault();
+        }
+    });
+});
+
