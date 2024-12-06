49b18ae6e297d87b00167bd7080a61fe88243f8f
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-12-06 13:46:11 +0100

    fix

---
+++ aplikacjaMVC/src/main/resources/static/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#rentForm1");
+    if (!form) {
+        return;
+    }
+
+    form.addEventListener("submit", function (event) {
+        const userConfirmed = confirm("OK?");
+        if (!userConfirmed) {
+            event.preventDefault();
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#rentForm2");
+    if (!form) {
+        return;
+    }
+
+    form.addEventListener("submit", function (event) {
+        const userConfirmed = confirm("OK?");
+        if (!userConfirmed) {
+            event.preventDefault();
+        }
+    });
+});
