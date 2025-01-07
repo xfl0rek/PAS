49e93bd9d6526ba9aa5ab286aa2ec2c05afa7397
Wojtyla23 <247734@edu.p.lodz.pl>
2025-01-07 19:17:34 +0100

    Tabela

--- SPA/src/components/UserTable.tsx
+++ SPA/src/components/UserTable.tsx
@@ -1,73 +1,97 @@
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useEffect, useState } from "react";
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
 // import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
 
+const UserTable = ({ users }: { users: User[] }) => {
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [search, setSearch] = useState<string>("");
 
-const UserTable = ({
-    users,
-} : {
-    users: User[];
-}) => {
-    const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
-    const [search, setSearch] = useState<string>("");
+  useEffect(() => {
+    let filtered = users;
 
-    useEffect(() => {
-        let filtered = users;
+    if (search) {
+      filtered = filtered.filter((user) =>
+        user.username.toLowerCase().includes(search.toLowerCase())
+      );
+    }
 
-        if (search) {
-            filtered = filtered.filter((user) => 
-            user.username.toLowerCase().includes(search.toLowerCase())
-            );
-        }
+    setFilteredUsers(filtered);
+  }, [search, users]);
 
-        setFilteredUsers(filtered);
-    }, [search, users] );
+  const activeHandler = async (user: User) => {
+    const url = user.active ? `/api/users/deactivateAccount/${user.id}` : `/api/users/activateAccount/${user.id}`;
+    const respone = await fetch(url, {
+        method: 'POST',
+    });
+    if (respone.ok) {
+        setFilteredUsers((prevUsers) =>
+            prevUsers.map((u) =>
+              u.id === user.id ? { ...u, active: !u.active } : u
+            )
+          );
+    }
+  };
 
-    return (
-        <div className="flex flex-col  items-center ">
-            <div>
-                {/* <Label>
+  return (
+    <div className="flex flex-col  m-4 min-h-screen">
+      <div className="flex flex-col items-center w-1/4 mb-4">
+        {/* <Label>
                     Wprowadz nazwe uzytkownika
                 </Label> */}
-                <Input type="text" placeholder="Nazwa uzytkownika" value={search} onChange={(e) => setSearch(e.target.value)} />
-            </div>
-            <Table>
-                <TableCaption>Lista klientow</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Nazwa uzytkownika</TableHead>
-                        <TableHead>Imie</TableHead>
-                        <TableHead>Nazwisko</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {filteredUsers.length > 0 ? (
-                        filteredUsers.map((user) => (
-                            <TableRow key={user.username}>
-                                <TableCell>{user.username}</TableCell>
-                                <TableCell>{user.firstName}</TableCell>
-                                <TableCell>{user.lastName}</TableCell>
-                            </TableRow>
-                            )
-                        )
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={3}>Brak uzytkownikow</TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </div>
-    );
+        <Input
+          type="text"
+          placeholder="Nazwa uzytkownika"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {/* <div className="w-full flex items-center"> */}
+      <Table className="table-auto justify-self-center">
+        <TableCaption>Lista klientow</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="px-20 py=10">Nazwa uzytkownika</TableHead>
+            <TableHead className="px-20 py=10">Imie</TableHead>
+            <TableHead className="px-20 py=10">Nazwisko</TableHead>
+            <TableHead className="px-20 py=10">Aktywny</TableHead>
+            <TableHead className="px-20 py=10"></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredUsers.length > 0 ? (
+            filteredUsers.map((user) => (
+              <TableRow key={user.username}>
+                <TableCell className="px-20 py=10">{user.username}</TableCell>
+                <TableCell className="px-20 py=10">{user.firstName}</TableCell>
+                <TableCell className="px-20 py=10">{user.lastName}</TableCell>
+                <TableCell className="px-20 py=10">{`${user.active}`}</TableCell>
+                <TableCell className="px-20 py=10">
+                  <Button onClick={() => activeHandler(user)}>
+                    {user.active ? "Dezaktywuj" : "Aktywuj"}
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={5}>Brak uzytkownikow</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default UserTable;
--- SPA/src/types/index.d.ts
+++ SPA/src/types/index.d.ts
@@ -1,5 +1,7 @@
 type User = {
+    id: string,
     username: string,
     firstName: string,
     lastName: string,
+    active: boolean,
 }
