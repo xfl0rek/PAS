d9fb59d4c28d3c54200528f015d04311e578eee5
Wojtyla23 <247734@edu.p.lodz.pl>
2025-01-16 20:35:29 +0100

    User table for mobile

--- SPA/src/pages/Rooms.tsx
+++ SPA/src/pages/Rooms.tsx
@@ -4,16 +4,18 @@ import Sidebar from "@/components/Sidebar";
 const Rooms = () => {
   return (
     <div className="flex h-screen">
-      <div className="w-40 h-full">
+      <div className="w-40 sm:w-40 h-full">
         <Sidebar />
       </div>
-      <div>
+      <div className="flex-grow overflow-auto">
         <p className="text-center">
           {localStorage.getItem("username") === null
             ? "Not logged in"
             : "Logged in as " + localStorage.getItem("username")}
         </p>
+        <div className="overflow-auto">
         <RoomList />
+        </div>
       </div>
     </div>
   );
